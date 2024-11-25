f41fb7d0a9b6eea3e5453e50b0c949778e723805
Shivam Goyal <151446208+shivamgoyal9119@users.noreply.github.com>
2024-11-25 13:35:53 +0530

    Update avlTree.cpp
    Insertion & Deletion

--- avlTree.cpp
+++ avlTree.cpp
@@ -13,6 +13,8 @@ class avl_tree_node{
     }
 };
 
+/* ************************************************************************************* */
+
 int getHeight(avl_tree_node* root){
     if(root == NULL) return 0;
     int l = 0, r = 0;
@@ -85,13 +87,76 @@ void get_preorder_traversal(avl_tree_node *root){
     get_preorder_traversal(root->right);
 }
 
+/* ************************************************************************************* */
+
+int get_min_ele(avl_tree_node* root){
+    if(root == NULL) return INT_MAX;
+    int ans = root->data;
+    ans = min(ans, get_min_ele(root->left));
+    ans = min(ans, get_min_ele(root->right));
+    return ans;
+}
+
+avl_tree_node* get_balance_node(avl_tree_node* root){
+    int h = getHeight(root->left), h2 = getHeight(root->right);
+    if(h-h2 >= -1 && h-h2 <= 1) return root;
+
+    if(h > h2){ // LL or LR
+        int l_h = getHeight(root->left->left), l_h2 = getHeight(root->left->right);
+        if(l_h >= l_h2) // LL
+            return make_ll(root);
+        else{ // LR
+            avl_tree_node* left_sub_tree = make_rr(root->left);
+            root->left = left_sub_tree;
+            // root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+            return make_ll(root);
+        }
+    }else{ // RR or RL
+        int r_h = getHeight(root->right->left), r_h2 = getHeight(root->right->right);
+        if(r_h > r_h2){ // RL
+            avl_tree_node* right_sub_tree = make_ll(root->right);
+            root->right = right_sub_tree;
+            // root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+            return make_rr(root);
+        }else // RR
+            return make_rr(root);
+    }
+}
+
+avl_tree_node* delete_(avl_tree_node* root, int t){
+    if(root->data == t){
+        if(root->left == NULL && root->right == NULL) return NULL;
+        else if(root->left == NULL && root->right != NULL) return root->right;
+        else if(root->left != NULL && root->right == NULL) return root->left;
+
+        int min_ele = get_min_ele(root->right);
+        root->data = min_ele;
+        root->right = delete_(root->right, min_ele);
+        root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+        return get_balance_node(root);
+    }
+
+    if(root->data > t){
+        root->left = delete_(root->left, t);
+        root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+        return get_balance_node(root);
+    }else{
+        root->right = delete_(root->right, t);
+        root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+        return get_balance_node(root);
+    }
+}
+
+/* ************************************************************************************* */
+
 int main(){
 
     // AVL Tree
     // -> Duplicate elements are not allowed, because its also a advanced version of BST.
 
-    // Insertion
+    // --- Fully Tested ---
 
+    // Insertion
     avl_tree_node *root = new avl_tree_node(21);
     root = insert(root, 26);
     root = insert(root, 30);
@@ -101,9 +166,11 @@ int main(){
     root = insert(root, 28);
 
     get_preorder_traversal(root);
+    cout<<endl;
 
-
-
+    // Deletion
+    root = delete_(root, 21);
+    get_preorder_traversal(root);
 
 
     return 0;
